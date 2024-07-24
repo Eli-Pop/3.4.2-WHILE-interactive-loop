50aa4752018ba8033514080c21177f24d962b0fd
Elias V. <58016431+Eli-Pop@users.noreply.github.com>
2024-07-23 21:16:54 -0500

    Add files via upload

---
+++ 3.4.2 WHILE interactive loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int len;
+
+	cout << "Enter length (1 to 79): ";
+	cin >> len;
+
+	while (len > 0 && len < 80)
+	{
+		cout << '.';
+		len--;
+	}
+
+	cout << "\n";
+	system("pause");
+	return 0;
+}
